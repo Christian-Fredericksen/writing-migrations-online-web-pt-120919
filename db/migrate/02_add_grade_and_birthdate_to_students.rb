86646eb327ed7af622316fd9e64f4859dd7b4220
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-02 23:03:04 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
-      t.integer :grade
-      t.string :birthdate
+    add_column :students do |c|
+      caches_page.integer :grade
+      caches_page.string :birthdate
     end
   end
 end 
